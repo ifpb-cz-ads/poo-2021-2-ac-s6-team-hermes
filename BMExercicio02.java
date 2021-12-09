86531ac381e6fdd1e5987ecc290ebd2bdf8a88d3
Hermes Feitoza Barros <57917124+WinterFB@users.noreply.github.com>
2021-12-08 21:27:49 -0300

    Create BMExercicio02.java

---
+++ BMExercicio02.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class DiaDaSemanaLaço
+{
+    public static void main(String[] args){
+        int dia;
+        do {
+            System.out.print("Digite o numero: ");
+            dia = new Scanner(System.in).nextInt();
+            switch (dia){
+            case 1: System.out.println("Domingo");
+                    break;
+            case 2: System.out.println("Segunda-feira");
+                    break;
+            case 3: System.out.println("Terça-feira");
+                    break;
+            case 4: System.out.println("Quarta-feira");
+                    break;
+            case 5: System.out.println("Quinta-feira");
+                    break;
+            case 6: System.out.println("Sexta-feira");
+                    break;
+            case 7: System.out.println("Sábado");
+                    break;
+            }
+        } while (dia != 0);
+    }
+}
