a4179646b0d645a9d2d1ff4af5f6e8a20c095c99
Hermes Feitoza Barros <57917124+WinterFB@users.noreply.github.com>
2021-12-08 21:55:36 -0300

    Create BMExercicio09.java

---
+++ BMExercicio09.java
@@ -0,0 +1,10 @@
+public class Imprimir20x
+{
+    public static void main(String[] args){
+        int x=10;
+        while (x<30){
+            x++;
+            System.out.println("" +x);
+        }
+    }
+}
